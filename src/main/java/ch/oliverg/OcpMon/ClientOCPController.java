c134d92d65b40e7628495d2965336a0745669612
guo <guo@optimus-pro15-6.home>
2018-03-31 21:54:49 +0200

    inplaying wiht ResponseBody

--- src/main/java/ch/oliverg/OcpMon/ClientOCPController.java
+++ src/main/java/ch/oliverg/OcpMon/ClientOCPController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 
@@ -24,6 +26,25 @@ public class ClientOCPController {
         return "ocp";
     }
 
+    @ResponseBody
+    @RequestMapping(value ="/txt" )
+    public String txtResponse(HttpServletResponse response){
+        //String fileName = "a.txt";
+        //response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+        response.setHeader("Content-Type", "text/plain");
+        String content = "This is txt content";
+        return content;
+    }
 
+
+    @ResponseBody
+    @RequestMapping(value ="/yaml" )
+    public String yamlResponse(HttpServletResponse response){
+        //String fileName = "a.txt";
+        //response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+        response.setHeader("Content-Type", "application/yaml");
+        String content = "This is txt content";
+        return content;
+    }
 
 }
